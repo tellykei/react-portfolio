c49a7c85ffa281c9a25a3c3c57e93a19debb18ac
Taliyah Rivera <trive2@bitbucket.org>
2019-12-02 21:02:19 -0500

    more frontend

--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -5,33 +5,36 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 
 var sectionStyle = {
-    width: '100%',
-    height: "42rem",
+    width: '95.5%',
+    height: "40rem",
     backgroundImage: "url("+'https://i2.wp.com/kamratalperiti.org/wp-content/uploads/corporate-buildings-m.jpg?ssl=1' + ")",
     backgroundRepeat: "noRepeat",
-    
+    align:'center',
+    padding:'2rem'
+  };
+var paper= {
+    margin:'2rem',
+    backgroundColor: "lightgray",
   };
 class HomePage extends React.Component {
     
     render(){
         return(
             <div style = {{margin : '2rem'}} >
-                <div style = {sectionStyle}>
-                    <Grid container spacing = {4} 
-                    >
-                        <Grid item xs = {12}>
-                            <img  margin = '5rem' width = '400rem' height = '400rem' src= "https://space-facts.com/wp-content/uploads/saturn.png"/>
-                        </Grid>                        
-
-                   
-            
-                        <Grid item xs={8} sm container>
-                        <Paper >xs=6 sm=3</Paper>
-                        </Grid>
-                    </Grid> 
+                <div style = {sectionStyle} >
+                    <Paper style = {paper}>
+                        <Grid container spacing = {3}>
+                            <Grid item xs = {8}>
+                                <img  width = '350rem' height = '350rem' src= "https://space-facts.com/wp-content/uploads/saturn.png"/>
+                            </Grid>                       
+                        
+                            <Grid item xs={8} >
+                                <Paper >xs=6 sm=3</Paper>
+                            </Grid>
+                        </Grid> 
+                    </Paper>
                 </div>
             </div>
-
         )
     }
 
